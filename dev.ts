51e774565fc764d765267dcca974c076bfbd4f9a
nik <nikita.nodejs@gmail.com>
2023-10-18 13:44:49 +0200

    .env

--- dev.ts
+++ dev.ts
@@ -5,4 +5,10 @@ import config from "./fresh.config.ts";
 
 import "$std/dotenv/load.ts";
 
-await dev(import.meta.url, "./main.ts", config);
+await dev(import.meta.url, "./main.ts", {
+  ...config,
+  server: {
+    ...config.server,
+    port: +(Deno.env.get("PORT") || 8080),
+  },
+});
